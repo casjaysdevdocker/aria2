d0397e44c4a3b28fff0a1dec48fe9522c5b3f7c9
casjay <git-admin@casjaysdev.com>
2023-02-24 04:09:18 -0500

    🗃️ Committing everything that changed 🗃️

--- Dockerfile
+++ Dockerfile
@@ -1,92 +1,140 @@
-FROM casjaysdevdocker/alpine:latest AS build
+# Docker image for aria2 using alpine template
+ARG LICENSE="MIT"
+ARG IMAGE_NAME="aria2"
+ARG PHP_SERVER="aria2"
+ARG TIMEZONE="America/New_York"
+ARG BUILD_DATE="Fri Feb 24 03:55:13 AM EST 2023"
+ARG DEFAULT_DATA_DIR="/usr/local/share/template-files/data"
+ARG DEFAULT_CONF_DIR="/usr/local/share/template-files/config"
+ARG DEFAULT_TEMPLATE_DIR="/usr/local/share/template-files/defaults"
 
-ARG ALPINE_VERSION="v3.16"
+ARG SERVICE_PORT="6800"
+ARG EXPOSE_PORTS="6800"
+ARG PHP_VERSION=""
+ARG NODE_VERSION="system"
+ARG NODE_MANAGER="system"
 ARG ARIANG_VERSION="1.2.4"
 
-ARG DEFAULT_DATA_DIR="/usr/local/share/template-files/data" \
-  DEFAULT_CONF_DIR="/usr/local/share/template-files/config" \
-  DEFAULT_TEMPLATE_DIR="/usr/local/share/template-files/defaults"
+ARG USER="root"
+ARG DISTRO_VERSION="3.17"
+ARG CONTAINER_VERSION="latest"
+ARG IMAGE_VERSION="${DISTRO_VERSION}"
+ARG BUILD_VERSION="${DISTRO_VERSION}"
 
-ARG PACK_LIST="bash aria2 unzip nginx"
+FROM casjaysdevdocker/alpine:${IMAGE_VERSION} AS build
+ARG USER
+ARG LICENSE
+ARG TIMEZONE
+ARG IMAGE_NAME
+ARG PHP_SERVER
+ARG BUILD_DATE
+ARG SERVICE_PORT
+ARG EXPOSE_PORTS
+ARG NODE_VERSION
+ARG NODE_MANAGER
+ARG BUILD_VERSION
+ARG DEFAULT_DATA_DIR
+ARG DEFAULT_CONF_DIR
+ARG DEFAULT_TEMPLATE_DIR
+ARG DISTRO_VERSION
+ARG PHP_VERSION
+ARG ARIANG_VERSION
 
-ENV LANG=en_US.utf8 \
-  ENV=ENV=~/.bashrc \
-  TZ="America/New_York" \
-  SHELL="/bin/sh" \
-  TERM="xterm-256color" \
-  TIMEZONE="${TZ:-$TIMEZONE}" \
-  HOSTNAME="casjaysdev-aria2"
+ARG PACK_LIST="bash iproute2 bash-completion ssmtp openssl wget curl jq ca-certificates tzdata mailcap \
+  git ncurses util-linux pciutils usbutils coreutils binutils findutils grep iproute2 sudo rsync zip certbot tini \
+  aria2 unzip nginx"
 
+ENV ENV=~/.bashrc
+ENV LANG=en_US.UTF-8
+ENV TZ="America/New_York"
+ENV SHELL="/bin/sh"
+ENV TERM="xterm-256color"
+ENV TIMEZONE="${TZ:-$TIMEZONE}"
+ENV HOSTNAME="casjaysdev-aria2"
+
+USER ${USER}
 COPY ./rootfs/. /
 
 RUN set -ex; \
   rm -Rf "/etc/apk/repositories"; \
+  [ "$DISTRO_VERSION" = "latest" ] && DISTRO_VERSION="edge"; \
+  [ "$DISTRO_VERSION" = "edge" ] || DISTRO_VERSION="v${DISTRO_VERSION}" ; \
   mkdir -p "${DEFAULT_DATA_DIR}" "${DEFAULT_CONF_DIR}" "${DEFAULT_TEMPLATE_DIR}"; \
-  echo "http://dl-cdn.alpinelinux.org/alpine/${ALPINE_VERSION}/main" >>"/etc/apk/repositories"; \
-  echo "http://dl-cdn.alpinelinux.org/alpine/${ALPINE_VERSION}/community" >>"/etc/apk/repositories"; \
-  if [ "${ALPINE_VERSION}" = "edge" ]; then echo "http://dl-cdn.alpinelinux.org/alpine/${ALPINE_VERSION}/testing" >>"/etc/apk/repositories" ; fi ; \
-  apk update --update-cache && apk add --no-cache ${PACK_LIST} && \
-  curl -q -LSsf https://github.com/mayswind/AriaNg/releases/download/$ARIANG_VERSION/AriaNg-$ARIANG_VERSION.zip -o /tmp/AriaNg-$ARIANG_VERSION.zip && \
+  echo "http://dl-cdn.alpinelinux.org/alpine/${DISTRO_VERSION}/main" >>"/etc/apk/repositories"; \
+  echo "http://dl-cdn.alpinelinux.org/alpine/${DISTRO_VERSION}/community" >>"/etc/apk/repositories"; \
+  if [ "${DISTRO_VERSION}" = "edge" ]; then echo "http://dl-cdn.alpinelinux.org/alpine/${DISTRO_VERSION}/testing" >>"/etc/apk/repositories" ; fi ; \
+  apk update --update-cache && apk add --no-cache ${PACK_LIST}
+
+RUN curl -q -LSsf https://github.com/mayswind/AriaNg/releases/download/$ARIANG_VERSION/AriaNg-$ARIANG_VERSION.zip -o /tmp/AriaNg-$ARIANG_VERSION.zip && \
   mkdir -p /var/www/ariang && unzip /tmp/AriaNg-$ARIANG_VERSION.zip -d /var/www/ariang 
 
 RUN echo 'Running cleanup' ; \
-  rm -Rf /usr/share/doc/* /usr/share/info/* /tmp/* /var/tmp/* /etc/nginx/* ; \
-  rm -Rf /usr/local/bin/.gitkeep /usr/local/bin/.gitkeep /config /data /var/cache/apk/* ; \
-  rm -rf /lib/systemd/system/multi-user.target.wants/* ; \
+  BASH_CMD="$(type -P bash)" ; \
   rm -rf /etc/systemd/system/*.wants/* ; \
+  rm -rf /lib/systemd/system/systemd-update-utmp* ; \
+  [ -f "BASH_CMD" ] && ln -sf $BASH_CMD /usr/bin/sh ; \
   rm -rf /lib/systemd/system/local-fs.target.wants/* ; \
+  rm -rf /lib/systemd/system/multi-user.target.wants/* ; \
   rm -rf /lib/systemd/system/sockets.target.wants/*udev* ; \
   rm -rf /lib/systemd/system/sockets.target.wants/*initctl* ; \
+  rm -Rf /usr/share/doc/* /usr/share/info/* /tmp/* /var/tmp/* ; \
   rm -rf /lib/systemd/system/sysinit.target.wants/systemd-tmpfiles-setup* ; \
-  rm -rf /lib/systemd/system/systemd-update-utmp* ; \
+  rm -Rf /usr/local/bin/.gitkeep /usr/local/bin/.gitkeep /config /data /var/cache/apk/* ; \
   if [ -d "/lib/systemd/system/sysinit.target.wants" ]; then cd "/lib/systemd/system/sysinit.target.wants" && rm $(ls | grep -v systemd-tmpfiles-setup) ; fi
 
 FROM scratch
+ARG USER
+ARG LICENSE
+ARG TIMEZONE
+ARG IMAGE_NAME
+ARG PHP_SERVER
+ARG BUILD_DATE
+ARG SERVICE_PORT
+ARG EXPOSE_PORTS
+ARG NODE_VERSION
+ARG NODE_MANAGER
+ARG BUILD_VERSION
+ARG DEFAULT_DATA_DIR
+ARG DEFAULT_CONF_DIR
+ARG DEFAULT_TEMPLATE_DIR
+ARG DISTRO_VERSION
+ARG PHP_VERSION
 
-ARG \
-  SERVICE_PORT="6800" \
-  EXPOSE_PORTS="6800" \
-  PHP_SERVER="aria2" \
-  NODE_VERSION="system" \
-  NODE_MANAGER="system" \
-  BUILD_VERSION="latest" \
-  LICENSE="MIT" \
-  IMAGE_NAME="aria2" \
-  BUILD_DATE="Wed Oct 19 05:13:52 PM EDT 2022" \
-  TIMEZONE="America/New_York"
+USER ${USER}
+WORKDIR /data/aria2
 
-LABEL maintainer="CasjaysDev <docker-admin@casjaysdev.com>" \
-  org.opencontainers.image.vendor="CasjaysDev" \
-  org.opencontainers.image.authors="CasjaysDev" \
-  org.opencontainers.image.vcs-type="Git" \
-  org.opencontainers.image.name="${IMAGE_NAME}" \
-  org.opencontainers.image.base.name="${IMAGE_NAME}" \
-  org.opencontainers.image.license="${LICENSE}" \
-  org.opencontainers.image.vcs-ref="${BUILD_VERSION}" \
-  org.opencontainers.image.build-date="${BUILD_DATE}" \
-  org.opencontainers.image.version="${BUILD_VERSION}" \
-  org.opencontainers.image.schema-version="${BUILD_VERSION}" \
-  org.opencontainers.image.url="https://hub.docker.com/r/casjaysdevdocker/${IMAGE_NAME}" \
-  org.opencontainers.image.vcs-url="https://github.com/casjaysdevdocker/${IMAGE_NAME}" \
-  org.opencontainers.image.url.source="https://github.com/casjaysdevdocker/${IMAGE_NAME}" \
-  org.opencontainers.image.documentation="https://hub.docker.com/r/casjaysdevdocker/${IMAGE_NAME}" \
-  org.opencontainers.image.description="Containerized version of ${IMAGE_NAME}"
+LABEL maintainer="CasjaysDev <docker-admin@casjaysdev.com>"
+LABEL org.opencontainers.image.vendor="CasjaysDev"
+LABEL org.opencontainers.image.authors="CasjaysDev"
+LABEL org.opencontainers.image.vcs-type="Git"
+LABEL org.opencontainers.image.name="${IMAGE_NAME}"
+LABEL org.opencontainers.image.base.name="${IMAGE_NAME}"
+LABEL org.opencontainers.image.license="${LICENSE}"
+LABEL org.opencontainers.image.vcs-ref="${BUILD_VERSION}"
+LABEL org.opencontainers.image.build-date="${BUILD_DATE}"
+LABEL org.opencontainers.image.version="${BUILD_VERSION}"
+LABEL org.opencontainers.image.schema-version="${BUILD_VERSION}"
+LABEL org.opencontainers.image.url="https://hub.docker.com/r/casjaysdevdocker/${IMAGE_NAME}"
+LABEL org.opencontainers.image.vcs-url="https://github.com/casjaysdevdocker/${IMAGE_NAME}"
+LABEL org.opencontainers.image.url.source="https://github.com/casjaysdevdocker/${IMAGE_NAME}"
+LABEL org.opencontainers.image.documentation="https://hub.docker.com/r/casjaysdevdocker/${IMAGE_NAME}"
+LABEL org.opencontainers.image.description="Containerized version of ${IMAGE_NAME}"
+LABEL com.github.containers.toolbox="false"
 
-ENV LANG=en_US.utf8 \
-  ENV=~/.bashrc \
-  SHELL="/bin/bash" \
-  PORT="${SERVICE_PORT}" \
-  TERM="xterm-256color" \
-  PHP_SERVER="${PHP_SERVER}" \
-  CONTAINER_NAME="${IMAGE_NAME}" \
-  TZ="${TZ:-America/New_York}" \
-  TIMEZONE="${TZ:-$TIMEZONE}" \
-  HOSTNAME="casjaysdev-${IMAGE_NAME}"
+ENV ENV=~/.bashrc
+ENV LANG=en_US.UTF-8
+ENV SHELL="/bin/bash"
+ENV PORT="${SERVICE_PORT}"
+ENV TERM="xterm-256color"
+ENV PHP_SERVER="${PHP_SERVER}"
+ENV PHP_VERSION="${PHP_VERSION}"
+ENV CONTAINER_NAME="${IMAGE_NAME}"
+ENV TZ="${TZ:-America/New_York}"
+ENV TIMEZONE="${TZ:-$TIMEZONE}"
+ENV HOSTNAME="casjaysdev-${IMAGE_NAME}"
+ENV USER="${USER}"
 
 COPY --from=build /. /
-
-USER root
-WORKDIR /data/aria2
 
 VOLUME [ "/config","/data" ]
 
--- rootfs/usr/local/bin/start-aria2.sh
+++ rootfs/usr/local/bin/start-aria2.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 # shellcheck shell=bash
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-##@Version           :  202210191713-git
+##@Version           :  202302240401-git
 # @@Author           :  Jason Hempstead
 # @@Contact          :  jason@casjaysdev.com
-# @@License          :  LICENSE.md
+# @@License          :  WTFPL
 # @@ReadME           :  start-aria2.sh --help
-# @@Copyright        :  Copyright: (c) 2022 Jason Hempstead, Casjays Developments
-# @@Created          :  Wednesday, Oct 19, 2022 17:13 EDT
+# @@Copyright        :  Copyright: (c) 2023 Jason Hempstead, Casjays Developments
+# @@Created          :  Friday, Feb 24, 2023 04:01 EST
 # @@File             :  start-aria2.sh
 # @@Description      :  script to start aria2
 # @@Changelog        :  New script
@@ -18,26 +18,84 @@
 # @@sudo/root        :  no
 # @@Template         :  other/start-service
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# Set trap
+trap -- 'retVal=$?;kill -9 $$;exit $retVal' SIGINT SIGTERM ERR EXIT
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 # Set functions
-__pcheck() { [ -n "$(which pgrep 2>/dev/null)" ] && pgrep -x "$1" || return 1; }
+__cd() { [ -d "$1" ] && builtin cd "$1" || return 1; }
+__curl() { curl -q -LSsf -o /dev/null "$@" &>/dev/null || return 10; }
 __find() { find "$1" -mindepth 1 -type ${2:-f,d} 2>/dev/null | grep '^' || return 10; }
-__curl() { curl -q -LSsf -o /dev/null -s -w "200" "$@" 2>/dev/null || return 10; }
+__pcheck() { [ -n "$(which pgrep 2>/dev/null)" ] && pgrep -x "$1" &>/dev/null || return 10; }
 __pgrep() { __pcheck "$1" || ps aux 2>/dev/null | grep -Fw " $1" | grep -qv ' grep' || return 10; }
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 __certbot() {
-  [ -n "$DOMANNAME" ] && [ -n "$CERT_BOT_MAIL" ] || { echo "The variables DOMANNAME and CERT_BOT_MAIL are set" && exit 1; }
+  [ -n "$DOMAINNAME" ] && [ -n "$CERT_BOT_MAIL" ] || { echo "The variables DOMAINNAME and CERT_BOT_MAIL are set" && exit 1; }
   [ "$SSL_CERT_BOT" = "true" ] && type -P certbot &>/dev/null || { export SSL_CERT_BOT="" && return 10; }
-  certbot $1 --agree-tos -m $CERT_BOT_MAIL certonly --webroot -w "${WWW_ROOT_DIR:-/data/htdocs/www}" -d $DOMAINNAME -d $DOMAINNAME \
+  certbot $1 --agree-tos -m $CERT_BOT_MAIL certonly --webroot \
+    -w "${WWW_ROOT_DIR:-/data/htdocs/www}" -d $DOMAINNAME -d $DOMAINNAME \
     --put-all-related-files-into "$SSL_DIR" -key-path "$SSL_KEY" -fullchain-path "$SSL_CERT"
+  return $?
 }
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 __heath_check() {
-  status=0 health="Good"
-  __pgrep ${1:-} || status=$((status + 1))
-  __curl "http://localhost:$ARIA2RPCPORT" || status=$((status + 1))
-  [ "$status" -eq 0 ] || health="Errors reported see docker logs --follow $CONTAINER_NAME"
-  echo "$(uname -s) $(uname -m) is running and the health is: $health"
-  return ${status:-$?}
+  local healthStatus=0 health="Good"
+  #__pgrep ${1:-$SERVICE_NAME} &>/dev/null || healthStatus=$((healthStatus + 1))
+  #__curl "http://localhost:$SERVICE_PORT/server-health" || healthStatus=$((healthStatus + 1))
+  [ "$healthStatus" -eq 0 ] || health="Errors reported see docker logs --follow $CONTAINER_NAME"
+  return $healthStatus
+}
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+__exec_command() {
+  local exitCode=0
+  local cmd="${*:-bash -l}"
+  echo "Executing: $cmd"
+  eval $cmd || exitCode=1
+  [ "$exitCode" = 0 ] || exitCode=10
+  return $exitCode
+}
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+__exec_service_start() {
+  [ -n "$DEBUG" ] && set -x
+  local exitCode=0 cmd="${SERVICE_COMMAND:-false}"
+  echo "Setting up service to run as $SERVICE_USER"
+  echo "Executing: $cmd "
+  if [ "$SERVICE_USER" = "root" ]; then
+    su_cmd() { eval "$@" || return 1; }
+  elif [ "$(builtin type -P su)" ]; then
+    su_cmd() { su -s /bin/sh - $SERVICE_USER -c "$@" || return 1; }
+  elif [ "$(builtin type -P runuser)" ]; then
+    su_cmd() { runuser -u $SERVICE_USER "$@" || return 1; }
+  elif [ "$(builtin type -P sudo)" ]; then
+    su_cmd() { sudo -u $SERVICE_USER "$@" || return 1; }
+  else
+    echo "Can not switch to $SERVICE_USER"
+    exit 10
+  fi
+  su_cmd "$cmd" && su_cmd "touch /tmp/$SERVICE_NAME.pid" || exitCode=1
+  [ "$exitCode" -ne 0 ] && exitCode=10 && rm -Rf "/tmp/$SERVICE_NAME.pid"
+  return $exitCode
+}
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+__start_message() {
+  __pgrep "$SERVICE_NAME" && [ -f "/tmp/$SERVICE_NAME.pid" ] && echo "$SERVICE_NAME is running" && exit 0
+  if [ "$ENTRYPOINT_MESSAGE" = "false" ]; then
+    echo "Starting $SERVICE_NAME on port: $SERVICE_PORT"
+  else
+    echo "Starting $SERVICE_NAME on: $CONTAINER_IP_ADDRESS:$SERVICE_PORT"
+  fi
+}
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+__exec_pre_start() {
+  __start_message
+  nginx -c /etc/nginx/nginx.conf
+}
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+__run_backup() {
+  local save="" date=""
+  save="${1:-$BACKUP_DIR}"
+  date="$(date '+%Y%m%d-%H%M')"
+  tar cfvz "$save/$date.tar.gz" --exclude="$save" "/data" "/config"
+  return $?
 }
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 # Set variables
@@ -45,10 +103,8 @@ DISPLAY="${DISPLAY:-}"
 LANG="${LANG:-C.UTF-8}"
 DOMAINNAME="${DOMAINNAME:-}"
 TZ="${TZ:-America/New_York}"
-HTTP_PORT="${HTTP_PORT:-80}"
-HTTPS_PORT="${HTTPS_PORT:-}"
-SERVICE_PORT="${SERVICE_PORT:-$HTTP_PORT}"
-SERVICE_NAME="${CONTAINER_NAME:-}"
+PORT="${SERVICE_PORT:-$PORT}"
+PHP_VERSION="${PHP_VERSION//php/}"
 HOSTNAME="${HOSTNAME:-casjaysdev-aria2}"
 HOSTADMIN="${HOSTADMIN:-root@${DOMAINNAME:-$HOSTNAME}}"
 SSL_CERT_BOT="${SSL_CERT_BOT:-false}"
@@ -58,6 +114,7 @@ SSL_CA="${SSL_CA:-$SSL_DIR/ca.crt}"
 SSL_KEY="${SSL_KEY:-$SSL_DIR/server.key}"
 SSL_CERT="${SSL_CERT:-$SSL_DIR/server.crt}"
 SSL_CONTAINER_DIR="${SSL_CONTAINER_DIR:-/etc/ssl/CA}"
+BACKUP_DIR="${BACKUP_DIR:-/config/backup}"
 WWW_ROOT_DIR="${WWW_ROOT_DIR:-/data/htdocs}"
 LOCAL_BIN_DIR="${LOCAL_BIN_DIR:-/usr/local/bin}"
 DATA_DIR_INITIALIZED="${DATA_DIR_INITIALIZED:-}"
@@ -65,19 +122,38 @@ CONFIG_DIR_INITIALIZED="${CONFIG_DIR_INITIALIZED:-}"
 DEFAULT_DATA_DIR="${DEFAULT_DATA_DIR:-/usr/local/share/template-files/data}"
 DEFAULT_CONF_DIR="${DEFAULT_CONF_DIR:-/usr/local/share/template-files/config}"
 DEFAULT_TEMPLATE_DIR="${DEFAULT_TEMPLATE_DIR:-/usr/local/share/template-files/defaults}"
-CONTAINER_IP_ADDRESS="$(ip a 2>/dev/null | grep 'inet' | grep -v '127.0.0.1' | awk '{print $2}' | sed 's|/*||g')"
-[ -n "$HTTP_PORT" ] || [ -n "$HTTPS_PORT" ] || HTTP_PORT="$SERVICE_PORT"
-[ "$HTTPS_PORT" = "443" ] && HTTP_PORT="$HTTPS_PORT" && SSL_ENABLED="true"
+CONTAINER_IP_ADDRESS="$(ip a 2>/dev/null | grep 'inet' | grep -v '127.0.0.1' | awk '{print $2}' | sed 's|/.*||g')"
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# Custom variables
+
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 # Overwrite variables
-RPC_SECRET="${RPC_SECRET:-}"
-SERVICE_PORT=""
+WORKDIR=""
+SERVICE_PORT="$PORT"
 SERVICE_NAME="aria2"
-SERVICE_COMMAND="aria2c --conf-path=/config/aria2/aria2.conf"
-export exec_message="Starting $SERVICE_NAME on $CONTAINER_IP_ADDRESS:$SERVICE_PORT"
+SERVICE_USER="${SERVICE_USER:-root}"
+SERVICE_COMMAND="$SERVICE_NAME --conf-path=/config/aria2/aria2.conf"
+ARIA2RPCPORT="${ARIA2RPCPORT:-$SERVICE_PORT}"
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+[ "$SERVICE_PORT" = "443" ] && SSL_ENABLED="true"
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 # Pre copy commands
-
+if [ -f "/config/aria2/aria-ng.config.js" ]; then
+  get_config="$(ls -A /var/www/ariang/js/aria-ng-*.min.js | grep -v 'f1dd57abb9.min' | head -n1)"
+  rm -Rf "$get_config"
+  ln -sf "/config/aria2/aria-ng.config.js" "$get_config"
+  ln -sf "/config/aria2/aria-ng.config.js" "/var/www/ariang/js/aria-ng-f1dd57abb9.min.js"
+  [ -n "$CONTAINER_IP_ADDRESS" ] && sed "s|127.0.0.1|$CONTAINER_IP_ADDRESS|g" "/config/aria2/aria-ng.config.js"
+fi
+if [ -n "$RPC_SECRET" ]; then
+  echo "Changing rpc secret to $RPC_SECRET"
+  if grep -sq "rpc-secret=*" "/config/aria2/aria2.conf"; then
+    sed -i "s|secret: '',|secret: ''$RPC_SECRET'',|g" "/var/www/ariang/js/aria-ng.config.js"
+    sed -i "s|rpc-secret=.*|rpc-secret=$RPC_SECRET|g" "/config/aria2/aria2.conf"
+  else
+    echo "rpc-secret=$RPC_SECRET" >>"/config/aria2/aria2.conf"
+  fi
+fi
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 # Check if this is a new container
 [ -z "$DATA_DIR_INITIALIZED" ] && [ -f "/data/.docker_has_run" ] && DATA_DIR_INITIALIZED="true"
@@ -103,23 +179,13 @@ if [ "$DATA_DIR_INITIALIZED" = "false" ] && [ -d "$DEFAULT_DATA_DIR/data/htdocs"
   [ -d "/data" ] && cp -Rf "$DEFAULT_DATA_DIR/data/htdocs/." "$WWW_ROOT_DIR/" 2>/dev/null
 fi
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-# Post copy commands
-if [ -f "/config/aria2/aria-ng.config.js" ]; then
-  get_config="$(ls -A /var/www/ariang/js/aria-ng-*.min.js | grep -v 'f1dd57abb9.min' | head -n1)"
-  rm -Rf "$get_config"
-  ln -sf "/config/aria2/aria-ng.config.js" "$get_config"
-  ln -sf "/config/aria2/aria-ng.config.js" "/var/www/ariang/js/aria-ng-f1dd57abb9.min.js"
-  [ -n "$CONTAINER_IP_ADDRESS" ] && sed "s|127.0.0.1|$CONTAINER_IP_ADDRESS|g" "/config/aria2/aria-ng.config.js"
-fi
+# Create the backup dir
+[ -d "$BACKUP_DIR" ] || mkdir -p "$BACKUP_DIR"
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-if [ -n "$RPC_SECRET" ]; then
-  echo "Changing rpc secret to $RPC_SECRET"
-  if grep -sq "rpc-secret=*" "/config/aria2/aria2.conf"; then
-    sed -i "s|secret: '',|secret: ''$RPC_SECRET'',|g" "/var/www/ariang/js/aria-ng.config.js"
-    sed -i "s|rpc-secret=.*|rpc-secret=$RPC_SECRET|g" "/config/aria2/aria2.conf"
-  else
-    echo "rpc-secret=$RPC_SECRET" >>"/config/aria2/aria2.conf"
-  fi
+# Post copy commands
+if [ -n "$ARIA2RPCPORT" ] && [ "$ARIA2RPCPORT" != "6800" ]; then
+  echo "Changing rpc request port to $ARIA2RPCPORT"
+  sed -i "s|listen       .*|listen                               ${ARIA2RPCPORT}|g" "/etc/nginx/nginx.conf"
 fi
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 # Initialized
@@ -128,11 +194,14 @@ fi
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 # APP Variables overrides
 [ -f "/root/env.sh" ] && . "/root/env.sh"
-[ -f "/config/env.sh" ] && "/config/env.sh"
+[ -f "/config/env.sh" ] && . "/config/env.sh"
 [ -f "/config/.env.sh" ] && . "/config/.env.sh"
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 # Actions based on env
 
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# Change to working dir
+[ -n "$WORKDIR" ] && __cd "$WORKDIR"
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 # begin main app
 case "$1" in
@@ -140,6 +209,11 @@ healthcheck)
   shift 1
   __heath_check "${SERVICE_NAME:-bash}"
   exit $?
+  ;;
+
+backup)
+  shift 1
+  __run_backup "${1:-$BACKUP_DIR}"
   ;;
 
 certbot)
@@ -157,12 +231,7 @@ certbot)
   ;;
 
 *)
-  if __pgrep "$SERVICE_NAME" && [ ! -f "/tmp/$SERVICE_NAME.pid" ]; then
-    echo "$SERVICE_NAME is running"
-  else
-    touch "/tmp/$SERVICE_NAME.pid"
-    __exec_command "$SERVICE_COMMAND" || rm -Rf "/tmp/$SERVICE_NAME.pid"
-  fi
+  __exec_pre_start && __exec_service_start
   ;;
 esac
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
@@ -175,3 +244,4 @@ exitCode="${exitCode:-$?}"
 exit ${exitCode:-$?}
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 # end
+# ex: ts=2 sw=2 et filetype=sh
