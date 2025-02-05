c3664221adb4a8bf5abe21fa80c45be8fecd30d2
casjay <git-admin@casjaysdev.pro>
2025-02-04 21:48:34 -0500

    🗃️ Committing everything that changed 🗃️
    Dockerfile
    .env.scripts
    rootfs/root/

---
+++ rootfs/root/docker/setup/05-custom.sh
@@ -0,0 +1,46 @@
+#!/usr/bin/env bash
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+##@Version           :  202501061127-git
+# @@Author           :  CasjaysDev
+# @@Contact          :  CasjaysDev <docker-admin@casjaysdev.pro>
+# @@License          :  MIT
+# @@ReadME           :
+# @@Copyright        :  Copyright 2023 CasjaysDev
+# @@Created          :  Mon Aug 28 06:48:42 PM EDT 2023
+# @@File             :  05-custom.sh
+# @@Description      :  script to run custom
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# shellcheck shell=bash
+# shellcheck disable=SC2016
+# shellcheck disable=SC2031
+# shellcheck disable=SC2120
+# shellcheck disable=SC2155
+# shellcheck disable=SC2199
+# shellcheck disable=SC2317
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# Set bash options
+set -o pipefail
+[ "$DEBUGGER" = "on" ] && echo "Enabling debugging" && set -x$DEBUGGER_OPTIONS
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# Set env variables
+exitCode=0
+ARIANG_VERSION="${ARIANG_VERSION:-$(curl -q -LSsf https://api.github.com/repos/mayswind/AriaNg/releases | jq -r '.[].name' | sort -rV | head -n1 | sed 's|AriaNg ||g' | grep '^' || echo '1.3.9')}"
+ARIANG_ARCHIVE_FILE="https://github.com/mayswind/AriaNg/releases/download/$ARIANG_VERSION/AriaNg-$ARIANG_VERSION.zip"
+ARIANG_TEMP_FILE="/tmp/AriaNg.zip"
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# Predefined actions
+[ -d "/usr/local/share/ariang" ] || mkdir -p "/usr/local/share/ariang"
+if curl -q -LSsf "$ARIANG_ARCHIVE_FILE" -o "$ARIANG_TEMP_FILE"; then
+  unzip "$ARIANG_TEMP_FILE" -d "/usr/local/share/ariang"
+  exitCode=$?
+else
+  exitCode=1
+fi
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# Main script
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# Set the exit code
+#exitCode=$?
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+exit $exitCode
